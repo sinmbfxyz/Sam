b45e2506c5f52509d8c7de16d0d0b84396cd714a
rajsir007 <rajg2239@gmail.com>
2024-07-03 11:58:05 +0545

    changes made

--- 2ndsem/Practical1/No16.java
+++ 2ndsem/Practical1/No16.java
@@ -5,6 +5,7 @@ public class No16 {
         Scanner sc = new Scanner(System.in);
         System.out.println("1.BBA/BIM/BCA");
         System.out.println("2.BBS/BBM");
-        System.out.println("3.BSC/BCOM/BA");
+        System.out.println("Enter your choice");
+        int choice = sc.nextInt();
     }
 }
